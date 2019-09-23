9800e59f22c1e82a036e4468c6d086da74704531
Artem Petrov <artem343p@gmail.com>
2019-09-23 20:25:59 +0200

    ADDED flask functionality

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,17 @@
 from flask import render_template
 from app import app
+import plotting
+import os
 
-@app.route('/')
-@app.route('/index')
+
+@app.route("/")
+@app.route("/index")
 def index():
-    return render_template('index.html', title='Home')
+    return render_template("index.html", title="Home")
+
+
+@app.route("/map")
+def map():
+    folium_map = plotting.get_folium_map()
+    return folium_map.get_root().render()
+
