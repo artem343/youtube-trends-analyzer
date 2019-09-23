37122b53bfdd0c914cbc5ee06ebe88176a16fcd7
Artem Petrov <artem343p@gmail.com>
2019-09-23 20:26:20 +0200

    added dataframe conversion function

--- convert_to_df.py
+++ convert_to_df.py
@@ -4,28 +4,6 @@ import json
 import pandas as pd
 import numpy as np
 
-# class Video:
-#     """
-#     One youtube video with subtitles and data.
-#     """
-
-#     def __init__(self, locale, id, url, title, duration, text):
-#         self.id = id
-#         self.locale = locale
-#         self.url = url
-#         self.title = title
-#         self.duration = duration
-#         self.text = text
-
-#     def __repr__(self):
-#         print(f'<Video "{self.title[:10]}" from {self.locale} (id {self.id})>')
-
-#     def determine_text_topic(self):
-#         """
-
-#         """
-#         pass
-
 
 def create_df_videos():
     with open("locales.txt", "r") as locfile:
@@ -36,10 +14,9 @@ def create_df_videos():
         try:
             locale_df = create_df_videos_for_locale(locale[:-1])
             df_videos = df_videos.append(locale_df, ignore_index=True)
-        except Exception as e:
-            print(f"No json for {locale}. {e}")
+        except Exception:
+            print(f"No json for {locale}")
 
-    print(f"df_videos.shape = {df_videos.shape}")
     # df_videos.to_csv('videos.csv')
     return df_videos
 
@@ -54,13 +31,50 @@ def create_df_videos_for_locale(locale):
             "locale": locale,
             "id": video["id"],
             "text": video["text"],
-            "topics": [],
-            "main_topic": "",
+            "title": video["title"],
+            "categories": video["categories"],
         }
         df_video_locale = df_video_locale.append(new_row, ignore_index=True)
 
     return df_video_locale
 
 
+def create_df_for_plotting(save=True):
+    """
+    Prepares a dataframe for plotting 
+    """
+    df_videos = create_df_videos()
+    df = df_videos
+    df["categories"] = df["categories"].apply(lambda x: x[0])
+
+    df_unstacked = (
+        df.groupby(["locale", "categories"])["id"]
+        .count()
+        .unstack()
+        .fillna(0.0)
+        .astype(int)
+    )
+    df_unstacked = df_unstacked.div(df_unstacked.sum(axis=1), axis=0)
+    df_unstacked.index.name = "locale2"
+
+    df_locale = pd.read_csv("locales.csv")
+    df_locale.drop(["Numeric code"], axis=1, inplace=True)
+    df_locale.columns = ["name", "locale2", "locale3", "lat_avg", "lon_avg"]
+
+    for column in df_locale.columns:
+        df_locale[column] = df_locale[column].str.replace('"', "")
+        df_locale[column] = df_locale[column].str.strip()
+
+    df_locale.drop_duplicates("locale2", keep="last", inplace=True)
+    df_locale.set_index("locale2", inplace=True)
+
+    df = df_locale.merge(df_unstacked, left_index=True, right_index=True)
+
+    if save:
+        df.to_csv("main.csv")
+
+    return df
+
+
 if __name__ == "__main__":
-    create_df_videos()
+    df = create_df_for_plotting(save=True)
